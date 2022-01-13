eb66d14b76415e2f6fe20eb8bf0afd03026bde25
Diego J <diegojromerolopez@gmail.com>
2022-01-13 18:47:49 +0100

    Upgrade django-ws-include to 0.2.0

--- shop_list/urls.py
+++ shop_list/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from shop_list import views
 
 app_name = 'shop_list'
@@ -8,4 +9,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # /shop_list/5/
     path('<int:shop_list_id>/', views.view_shop_list, name='view_shop_list'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
